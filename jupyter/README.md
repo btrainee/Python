0db03957f78ee446f32b2875ea78571ca91262e8
lixiaobo <xiaobolisx@126.com>
2021-10-20 08:43:14 +0800

    update jupyter

---
+++ jupyter/README.md
@@ -0,0 +1,7 @@
+# Anoconda
+## Python
+python study
+## jupyter 
+jupyter study
+
+[![build status](https://travis-ci.org/jupyterlab/jupyterlab-demo.svg?branch=master)](https://travis-ci.org/jupyterlab/jupyterlab-demo)  
---
+++ jupyter/hello.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+'''
+Description:
+	this file is used to test
+'''
+import argparse
+import sys
+import os
+import pandas as pd
+from functools import reduce
+
+__author__='Lixb'
+__mail__='XXX'
+
+print ('hello!')
